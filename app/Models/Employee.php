d7e00af4a206e415b185c429fa9c61bec60f71e6
herrysabar <sabarudin.herry@gmail.com>
2025-07-22 14:51:32 +0700

    update contract

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -67,7 +67,7 @@ class Employee extends BaseModel
 
     public function activeContract(){
         $now = now();
-        $activeContract = $this->contracts->where('employee_status','Permanent')
+        $activeContract = $this->contracts->wherein('employee_status',['Permanent','Contract'])
                             ->whereNull('resign_date')
                             ->whereNotNull('join_date')
                             ->where('start_payroll','<=',$now->format('Y-m-d'))
